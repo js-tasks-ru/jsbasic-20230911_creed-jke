97d840ee664a195da105034f191e2dd2b1e167fb
Igor Polikarpov <creedjke94@gmail.com>
2023-09-15 00:36:28 +0300

    Added 1module 1task solution

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  
+  if (n === 0 || n === 1) {
+    return result;
+  } else {
+    for (i = n; i >= 2; i--) {
+      result *= i;
+    }
+  };
+
+  return result;
 }
