66decd82e95728e73c4d2b32396c79c931c9f91a
Igor Polikarpov <creedjke94@gmail.com>
2023-09-15 10:35:39 +0300

    Added 1st module 3rd task solution

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,9 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str.length === 1) {
+    return str.toUpperCase();
+  } else if (str === "") {
+    return str;
+  } else {
+    return str[0].toUpperCase() + str.slice(1);
+  };
 }
